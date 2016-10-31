a88165471e49bf6d966ab76ebf48206a6a0ec032
Colin <colinwmackenzie@gmail.com>
2016-10-30 22:36:51 -0400

    eod, got the paths tracing!

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,10 @@
-function SVGEl(element, total_frames) {
-    this.el = element;
-    this.current_frame = 0;
-    this.total_frames = total_frames;
-    this.path = [];
-    this.length = [];
-    this.handle = 0;
-    this.init();
-}
+var work = document.querySelector('#work');
+var workIcon = document.querySelector("#work-icon-container svg");
 
-SVGEl.prototype.init = function() {
-    var el = this;
-    [].slice.call(this.el.querySelectorAll("path")).forEach(function(t, n) {
-      e.path[n] = t;
-      var r = e.path[n].getTotalLength();
-      e.length[n] = r;
-      e.path[n].style.strokeDasharray = r + " " + r;
-      e.path[n].style.strokeDashoffset = r;
-  });
-};
+work.addEventListener("mouseenter", function() {
+  workIcon.classList.add('animate');
+});
 
-SVGEl.prototype.draw = function() {
-    var e = this
-      , t = this.current_frame / this.total_frames;
-    if (t > 1) {
-            window.cancelAnimFrame(this.handle)
-        } else {
-                this.current_frame++;
-                for (var n = 0, r = this.path.length; n < r; n++) {
-                            this.path[n].style.strokeDashoffset = Math.floor(this.length[n] * (1 - t))
-                        }
-                this.handle = window.requestAnimFrame(function() {
-                            e.draw()
-                        })
-            }
-}
-;
-define(["jquery"], function(e) {
-    window.requestAnimFrame = function() {
-            return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(e) {
-                        window.setTimeout(e, 1e3 / 60)
-                    }
-        }();
-    window.cancelAnimFrame = function() {
-            return window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.mozCancelAnimationFrame || window.oCancelAnimationFrame || window.msCancelAnimationFrame || function(e) {
-                        window.clearTimeout(e)
-                    }
-        }()
-})
-
+work.addEventListener("mouseleave", function() {
+  workIcon.classList.remove('animate');
+});
