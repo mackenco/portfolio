80f91a9cb780fc3cfe93bf4cec1da04b7b0e3622
Colin <colinwmackenzie@gmail.com>
2016-10-30 21:21:19 -0400

    working on the path stuff

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,8 +1,1 @@
-$(document).ready(function() {
-	$("body").on("click", "a.scroll", function(event) {
-		event.preventDefault();
-		$('html, body').animate({
-			scrollTop: $($(this).attr("href")).offset().top - 50
-		}, 400);
-	})
-})
+
