dd4a5c6a4588b77a4c2273b93af6549f69b055e9
Colin <colinwmackenzie@gmail.com>
2016-10-31 22:20:18 -0400

    transition out

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,23 @@ var workIcon = document.querySelector("#work-icon-container svg");
 var about = document.querySelector('#about');
 var aboutIcon = document.querySelector("#about-icon-container svg");
 
-var icons = [[work, workIcon], [about, aboutIcon]];
+var links = [[work, workIcon], [about, aboutIcon]];
 
-icons.forEach(function(i) {
-  i[0].addEventListener("mouseenter", function() {
-    i[1].classList.add('animate'); 
+var h1 = document.querySelector('h1');
+var h2 = document.querySelector('h2');
+
+links.forEach(function(link) {
+  link[0].addEventListener("mouseenter", function() {
+    link[1].classList.add('animate'); 
   });
 
-  i[0].addEventListener("mouseleave", function() {
-    i[1].classList.remove('animate'); 
+  link[0].addEventListener("mouseleave", function() {
+    link[1].classList.remove('animate'); 
   });
+
+  link[0].addEventListener("click", function() {
+    h1.classList.add('disappear-up'); 
+    h2.classList.add('disappear-down'); 
+  });
+
 });
