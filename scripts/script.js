1e3ecf814d0172810b19b3d81aadff1bd7d30080
Colin <colinwmackenzie@gmail.com>
2016-11-14 22:15:34 -0500

    eod, argh in the middle of stuff

--- scripts/script.js
+++ scripts/script.js
@@ -2,24 +2,28 @@
  * TODO: CSS lint, build process, webkit stuff
  */
 
-var work = document.querySelector('.work');
+var workLink = document.querySelector('.work-link');
 var workIcon = document.querySelector(".work-icon-container svg");
 
-var about = document.querySelector('.about');
+var aboutLink = document.querySelector('.about-link');
 var aboutIcon = document.querySelector(".about-icon-container svg");
 
 var aboutToHome = document.querySelector('.about-to-home');
 var workToHome = document.querySelector('.work-to-home');
 
-var links = [[work, workIcon], [about, aboutIcon]];
+var pairs = { about: 'work', work: 'about' };
+var links = [[workLink, workIcon], [aboutLink, aboutIcon]];
 var homeLinks = [aboutToHome, workToHome];
 
 var h1 = document.querySelector('h1');
 var h2 = document.querySelector('h2');
-var bio = document.querySelector('.bio');
 var selectedIndex = -1;
 
-links.forEach(function(link, i) {
+for (var section in pairs) { 
+  var l0 = document.querySelector('.' + pairs[section] + '-link');
+  var l1 = document.querySelector('.' + pairs[section] + '-icon-container svg');
+  var link = [l0, l1];
+// links.forEach(function(link, i) {
   link[0].addEventListener("mouseenter", function() {
     link[1].classList.add('animate'); 
   });
@@ -29,32 +33,36 @@ links.forEach(function(link, i) {
   });
 
   link[0].addEventListener("click", function() {
-    var otherID = link[0].id === 'about' ? '.work' : '.about';
+    var selectedSection = this.parentElement.id;
+    var otherSection = "." + pairs[selectedSection];
     selectedIndex = i;
 
     h1.classList.add('disappear-up'); 
     h2.classList.add('disappear-down'); 
     link[0].classList.add('link-stay');
     link[1].classList.remove('on-hover');
-    document.querySelector(otherID + '-to-home').classList.remove('hide');
-    document.querySelector(otherID).classList.add('inactive');
+    document.querySelector(otherSection+ '-to-home').classList.remove('hide');
+    document.querySelector(otherSection + '-link').classList.add('inactive');
+    content = document.querySelector('.' + selectedSection + '-content');
 
     setTimeout(function() { 
-      bio.classList.add('quick-fade');
-      bio.classList.remove('hide');
+      content.classList.add('quick-fade');
+      content.classList.remove('hide');
       h1.classList.add('hide');
       h1.classList.remove('disappear-up'); 
       h2.classList.remove('disappear-down'); 
       h2.classList.add('hide');
     }, 500);
   });
-});
+// });
+}
 
 homeLinks.forEach(function(link) {
   link.addEventListener('click', function() {
+    content = document.querySelector("." + pairs[this.parentElement.id] + "-content");
+    content.classList.add('hide');
     h1.classList.remove('hide');
     h2.classList.remove('hide');
-    bio.classList.add('hide');
     links[selectedIndex][0].classList.remove('link-stay');
     links[selectedIndex][1].classList.add('on-hover');
     document.querySelector('.inactive').classList.remove('inactive');
