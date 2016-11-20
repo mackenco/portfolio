282eb28f76997a224096515a03e28e7a34825d34
Colin <colinwmackenzie@gmail.com>
2016-11-20 01:01:53 -0500

    lots of js refactor

--- scripts/script.js
+++ scripts/script.js
@@ -2,69 +2,63 @@
  * TODO: CSS lint, build process, webkit stuff
  */
 
-var pairs = { about: 'work', work: 'about' };
-var links = [];
-var homeLinks = [];
-var h1 = document.querySelector('h1');
-var h2 = document.querySelector('h2');
+var h1Class = document.querySelector('h1').classList;
+var h2Class = document.querySelector('h2').classList;
+var sections = { 
+  about: { inverse: 'work'}, 
+  work: { inverse: 'about' }
+};
 
-Object.keys(pairs).forEach(function(section) {
-  var set = [];
-  set.push(document.querySelector('.' + section + '-link'));
-  set.push(document.querySelector('.' + section + '-icon-container svg'));
-  links.push(set);
-
-  homeLinks.push(document.querySelector('.' + section + '-to-home'));
+Object.keys(sections).forEach(function(section) {
+  sections[section].link = document.querySelector('.' + section + '-link');
+  sections[section].svg = document.querySelector('.' + section + '-icon-container svg');
+  sections[section].home = document.querySelector('.' + section + '-to-home');
+  sections[section].content = document.querySelector('.' + section + '-content');
 });
 
-Object.keys(pairs).forEach(function(section) { 
-  var l0 = document.querySelector('.' + pairs[section] + '-link');
-  var l1 = document.querySelector('.' + pairs[section] + '-icon-container svg');
-  var link = [l0, l1];
+Object.keys(sections).forEach(function(section) {
+  var sectionInfo = sections[section];
+  var link = sectionInfo.link;
+  var svg = sectionInfo.svg;
+  var content = sectionInfo.content;
+  var home = sectionInfo.home;
+  var inverseInfo = sections[sectionInfo.inverse];
 
-  link[0].addEventListener('mouseenter', function() {
-    link[1].classList.add('animate'); 
+  link.addEventListener('mouseenter', function() {
+    svg.classList.add('animate'); 
   });
 
-  link[0].addEventListener('mouseleave', function() {
-    link[1].classList.remove('animate'); 
+  link.addEventListener('mouseleave', function() {
+    svg.classList.remove('animate'); 
   });
 
-  link[0].addEventListener('click', function() {
-    var selectedSection = this.parentElement.id;
-    var otherSection = '.' + pairs[selectedSection];
-
-    h1.classList.add('disappear-up'); 
-    h2.classList.add('disappear-down'); 
-    link[0].classList.add('link-stay');
-    link[1].classList.remove('on-hover');
-    document.querySelector(otherSection+ '-to-home').classList.remove('hide');
-    document.querySelector(otherSection + '-link').classList.add('inactive');
-    content = document.querySelector('.' + selectedSection + '-content');
+  link.addEventListener('click', function() {
+    h1Class.add('disappear-up'); 
+    h2Class.add('disappear-down'); 
+    link.classList.add('link-stay');
+    svg.classList.remove('on-hover');
+    
+    inverseInfo.home.classList.remove('hide');
+    inverseInfo.link.classList.add('inactive');
 
     setTimeout(function() { 
       content.classList.add('quick-fade');
       content.classList.remove('hide');
-      h1.classList.add('hide');
-      h1.classList.remove('disappear-up'); 
-      h2.classList.remove('disappear-down'); 
-      h2.classList.add('hide');
+      h1Class.add('hide');
+      h1Class.remove('disappear-up'); 
+      h2Class.remove('disappear-down'); 
+      h2Class.add('hide');
     }, 500);
   });
-});
 
-homeLinks.forEach(function(link) {
-  link.addEventListener('click', function() {
-    var selectedSection = this.parentElement.id;
-    var otherSection = pairs[selectedSection];
+  home.addEventListener('click', function() { 
+    h1Class.remove('hide');
+    h2Class.remove('hide');
 
-    content = document.querySelector('.' + pairs[this.parentElement.id] + '-content');
-    content.classList.add('hide');
-    h1.classList.remove('hide');
-    h2.classList.remove('hide');
+    inverseInfo.content.classList.add('hide');
+    inverseInfo.link.classList.remove('link-stay');
+    inverseInfo.svg.classList.add('on-hover');
 
-    document.querySelector('.' + otherSection + '-link').classList.remove('link-stay');
-    document.querySelector('.' + otherSection + '-icon-container svg').classList.add('on-hover');
     document.querySelector('.inactive').classList.remove('inactive');
     this.classList.add('hide');
   });
