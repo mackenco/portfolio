1bb0a40e628c00fb2d0d1582da86dac998ea6592
Colin <colinwmackenzie@gmail.com>
2016-11-19 20:34:17 -0500

    js refactor looks good!

--- scripts/script.js
+++ scripts/script.js
@@ -2,40 +2,37 @@
  * TODO: CSS lint, build process, webkit stuff
  */
 
-var workLink = document.querySelector('.work-link');
-var workIcon = document.querySelector(".work-icon-container svg");
-
-var aboutLink = document.querySelector('.about-link');
-var aboutIcon = document.querySelector(".about-icon-container svg");
-
-var aboutToHome = document.querySelector('.about-to-home');
-var workToHome = document.querySelector('.work-to-home');
-
 var pairs = { about: 'work', work: 'about' };
-var links = [[workLink, workIcon], [aboutLink, aboutIcon]];
-var homeLinks = [aboutToHome, workToHome];
-
+var links = [];
+var homeLinks = [];
 var h1 = document.querySelector('h1');
 var h2 = document.querySelector('h2');
-var selectedIndex = -1;
 
-for (var section in pairs) { 
+Object.keys(pairs).forEach(function(section) {
+  var set = [];
+  set.push(document.querySelector('.' + section + '-link'));
+  set.push(document.querySelector('.' + section + '-icon-container svg'));
+  links.push(set);
+
+  homeLinks.push(document.querySelector('.' + section + '-to-home'));
+});
+
+Object.keys(pairs).forEach(function(section) { 
   var l0 = document.querySelector('.' + pairs[section] + '-link');
   var l1 = document.querySelector('.' + pairs[section] + '-icon-container svg');
   var link = [l0, l1];
-// links.forEach(function(link, i) {
-  link[0].addEventListener("mouseenter", function() {
+
+  link[0].addEventListener('mouseenter', function() {
     link[1].classList.add('animate'); 
   });
 
-  link[0].addEventListener("mouseleave", function() {
+  link[0].addEventListener('mouseleave', function() {
     link[1].classList.remove('animate'); 
   });
 
-  link[0].addEventListener("click", function() {
+  link[0].addEventListener('click', function() {
     var selectedSection = this.parentElement.id;
-    var otherSection = "." + pairs[selectedSection];
-    selectedIndex = i;
+    var otherSection = '.' + pairs[selectedSection];
 
     h1.classList.add('disappear-up'); 
     h2.classList.add('disappear-down'); 
@@ -54,17 +51,20 @@ for (var section in pairs) {
       h2.classList.add('hide');
     }, 500);
   });
-// });
-}
+});
 
 homeLinks.forEach(function(link) {
   link.addEventListener('click', function() {
-    content = document.querySelector("." + pairs[this.parentElement.id] + "-content");
+    var selectedSection = this.parentElement.id;
+    var otherSection = pairs[selectedSection];
+
+    content = document.querySelector('.' + pairs[this.parentElement.id] + '-content');
     content.classList.add('hide');
     h1.classList.remove('hide');
     h2.classList.remove('hide');
-    links[selectedIndex][0].classList.remove('link-stay');
-    links[selectedIndex][1].classList.add('on-hover');
+
+    document.querySelector('.' + otherSection + '-link').classList.remove('link-stay');
+    document.querySelector('.' + otherSection + '-icon-container svg').classList.add('on-hover');
     document.querySelector('.inactive').classList.remove('inactive');
     this.classList.add('hide');
   });
